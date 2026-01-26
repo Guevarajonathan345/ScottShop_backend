b56473e1ce234782e20dfd238db5cbebc86eb8ca
JonathanGuevara <jonathanguevara345@gmail.com>
2026-01-26 15:50:33 -0600

    produc route modified

--- routes/productos.js
+++ routes/productos.js
@@ -1,21 +1,21 @@
-import { Router } from 'express';
-import { getProductos, createProducto, updateProducto, deleteProducto } from '../controllers/productoController.js';
-import { validateProductCreation } from '../middleware/productoValidator.js'; 
-import { protect, admin } from '../middleware/authMiddleware.js';
+    import { Router } from 'express';
+    import { getProductos, createProducto, updateProducto, deleteProducto } from '../controllers/productoController.js';
+    import { validateProductCreation } from '../middleware/productoValidator.js'; 
+    import { protect, admin } from '../middleware/authMiddleware.js';
 
-const router = Router();
+    const router = Router();
 
-//RUTAS PROTEGIDAS (JWT)
+    //RUTAS PROTEGIDAS (JWT)
 
-/*router.get('/', protect, getProductos);
-router.post('/', protect, validateProductCreation, createProducto);
-router.put('/:id', protect, validateProductCreation, updateProducto);
-router.delete('/:id', protect, deleteProducto);*/
+    /*router.get('/', protect, getProductos);
+    router.post('/', protect, validateProductCreation, createProducto);
+    router.put('/:id', protect, validateProductCreation, updateProducto);
+    router.delete('/:id', protect, deleteProducto);*/
 
-router.get('/', protect, getProductos);
-router.post('/', protect, admin, validateProductCreation, createProducto);
-router.put('/:id', protect, admin, validateProductCreation, updateProducto);
-router.delete('/:id', protect, admin, deleteProducto);
+    router.get('/', protect, getProductos);
+    router.post('/', protect, admin, upload.single('imagen'), validateProductCreation, createProducto);
+    router.put('/:id', protect, admin, upload.single('imagen'), validateProductCreation, updateProducto);
+    router.delete('/:id', protect, admin, deleteProducto);
 
 
-export default router;
+    export default router;
