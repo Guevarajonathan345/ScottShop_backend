13b028d340d5d0ed3a738d167ee03fab50ed13d5
JonathanGuevara <jonathanguevara345@gmail.com>
2026-06-04 10:42:58 -0600

    new validator to variantes

---
+++ middlewares/varianteValidator.js
@@ -0,0 +1,20 @@
+import { body, validationResult } from 'express-validator';
+
+export const validateVariantesCreation = [
+    body('product_id').isInt().withMessage('ID de producto invalido o no entero'),
+    body('almacenamiento').notEmpty(),
+    body('ram').notEmpty(),
+    body('precio').isFloat({ gt: 0 }).withMessage('El precio debe ser un número positivo'),
+    body('stock').isInt({ min: 0 }),
+    body('sku').notEmpty(),
+
+    // Middleware que verifica y maneja los errores
+    (req, res , next) => {
+        const errors = validationResult (req);
+
+        if (!errors.isEmpty()){
+            return res.status(400).json ({errors: errors.array()})
+        }
+        next();
+    }
+];
